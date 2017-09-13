47208b122614899c2b6a6bf3838916aa22b57cef
Rafa SysAdmin Barroso <rafa@parlevelsystems.com>
2017-09-12 23:44:12 -0500

    more

--- test.php
+++ test.php
@@ -15,8 +15,7 @@ echo 'this is term:'.$term;
 // 	'projectId' => 'rbse-webserv',
 // ]);
 // Connect to CloudSQL from App Engine.
-// $dsn = getenv('MYSQL_DSN');
-$dsn = '35.188.253.244';
+$dsn = getenv('MYSQL_DSN');
 $user = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
 if (!isset($dsn, $user) || false === $password) {
